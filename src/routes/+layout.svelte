ee490f9fb260702f3ca3b5a2bc8a45acd7eac0db
lucasmonsan <lucasmonsan@gmail.com>
2026-02-25 23:29:15 -0300

    Initial personal filesystem

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import '$lib/styles/normalize.css';
+	import '$lib/styles/palette.css';
+	import '$lib/styles/typography.css';
+	import '$lib/styles/variables.css';
+
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+{@render children()}
